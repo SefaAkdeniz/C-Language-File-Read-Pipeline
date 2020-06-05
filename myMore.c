7ece8b0b5cba0f1860c4b4c50d95ab301e21205d
ismet kizgin <info@ismetkizgin.com>
2020-06-06 00:49:55 +0300

    feat: add myMore

---
+++ myMore.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    printf("%d\n",getpid());
+    //printf("%s", argv[1]);
+    //printf("asdasdasd");
+    return 0;
+}
