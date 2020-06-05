04f3f70b5c8721c4c00097e3ef47c1500cca622a
ismet kizgin <info@ismetkizgin.com>
2020-06-06 00:49:40 +0300

    fix: fix for myData

--- myData.c
+++ myData.c
@@ -1,23 +1,58 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <pthread.h>
 
-int main (int argc, char** argv)
+void reader(char **);
+
+int main(int argc, char **argv)
 {
    char filename[] = "";
    strcpy(filename, argv[1]);
-   FILE *file = fopen ( filename, "r" );
-   if ( file != NULL )
+   FILE *file = fopen(filename, "r");
+   if (file != NULL)
    {
-      char line [ 128 ];
-      while ( fgets ( line, sizeof line, file ) != NULL )
+      char line[128];
+
+      if (argv[2] == NULL)
       {
-         fputs ( line, stdout );
+         while (fgets(line, sizeof line, file) != NULL)
+         {
+            fputs(line, stdout);
+         }
       }
-      fclose ( file );
+      else if (!strcmp(argv[2], "="))
+      {
+         pid_t pid;
+         while (fgets(line, sizeof line, file) != NULL)
+         {
+            pid = fork();
+            if (pid < 0)
+            {
+               printf("fork error\n");
+            }
+            else if (pid > 0)
+            {
+               char *args[] = {argv[3], line};
+               execv(argv[3], args);
+               perror("execv");
+            }
+            else
+            {
+            }
+         }
+      }
+      else
+      {
+         printf("\nYanlis bir komut girdiniz !\n\n");
+      }
+
+      fclose(file);
    }
    else
    {
-      perror ( filename );
+      perror(filename);
    }
+
    return 0;
 }
