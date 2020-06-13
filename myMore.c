cdad2ee9e0a34bea90248710caf3aa4ba3a19ad1
ismet kizgin <info@ismetkizgin.com>
2020-06-14 02:16:38 +0300

    fix: fix for myMore.c

--- myMore.c
+++ myMore.c
@@ -1,11 +1,42 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+
+#define BufferSize 500
+#define MaxLineSize 250
+#define PrintLineCount 24
 
 int main(int argc, char *argv[])
 {
-    printf("%d\n",getpid());
-    //printf("%s", argv[1]);
-    //printf("asdasdasd");
-    return 0;
+	char pidIndex[10];
+	int pidValue = getpid();
+	sprintf(pidIndex, "%d", pidValue);
+
+	char ppidIndex[10];
+	int ppidValue = getppid();
+	sprintf(ppidIndex, "%d", ppidValue);
+
+	char *pidIndexParent = argv[2];
+	char *ppidIndexParent = argv[3];
+
+	if (strcmp(pidIndex, pidIndexParent) != 0 && strcmp(ppidIndex, ppidIndexParent) != 0)
+	{
+		printf("myMore dosyasi parenti olmadan calisamaz...\n");
+	}
+	else
+	{
+		int readEnd = atoi(argv[1]);
+		char readMessage[MaxLineSize][BufferSize];
+
+		read(readEnd, &readMessage, PrintLineCount * BufferSize);
+
+		for (int i = 0; i < PrintLineCount; ++i)
+		{
+			printf("%s", readMessage[i]);
+		}
+
+		close(readEnd);
+		return 0;
+	}
 }
